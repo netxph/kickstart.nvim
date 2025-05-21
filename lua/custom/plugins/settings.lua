817f15cbbd5758b78f61f2c157640469fe1fd2c3
Marc VITALIS <marc.vitalis@live.com>
2025-05-21 16:28:49 +0800

    add settings

--- lua/custom/plugins/settings.lua
+++ lua/custom/plugins/settings.lua
@@ -1,4 +1,15 @@
 vim.o.relativenumber = true
 vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.softtabstop = 4
+vim.o.cindent = true
+
+vim.diagnostic.config {
+  virtual_text = false,
+}
 
 return {}
